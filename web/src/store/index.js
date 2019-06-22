1ba4f048931c7aa6e59058e106415e8ab758d3d7
Paulo Eduardo <paulo.es@live.com>
2019-06-22 14:31:04 -0300

    commit with fake backend for dev purposes

--- web/src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { requestTools, searchField } from "./reducers";
-
-const rootReducers = combineReducers({ requestTools, searchField });
-
-export default rootReducers;
